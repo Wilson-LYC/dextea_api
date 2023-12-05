c14cb267f1c2922c46949c1e141800052f1adc20
wilson <wilson_lyc@foxmail.com>
2023-12-05 14:12:28 +0800

    更新数据库

--- src/main/resources/dextea.sql
+++ src/main/resources/dextea.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80034
  File Encoding         : 65001
 
- Date: 04/12/2023 09:33:38
+ Date: 05/12/2023 14:11:21
 */
 
 SET NAMES utf8mb4;
@@ -50,7 +50,7 @@ INSERT INTO `cate2comm` VALUES (1, 1, '2023-12-03 00:47:56');
 INSERT INTO `cate2comm` VALUES (1, 2, '2023-11-28 12:34:41');
 INSERT INTO `cate2comm` VALUES (2, 1, '2023-12-03 00:47:57');
 INSERT INTO `cate2comm` VALUES (2, 2, '2023-11-28 12:34:41');
-INSERT INTO `cate2comm` VALUES (2, 3, '2023-12-03 00:48:09');
+INSERT INTO `cate2comm` VALUES (2, 3, '2023-12-05 13:39:00');
 INSERT INTO `cate2comm` VALUES (4, 5, '2023-12-03 01:16:12');
 
 -- ----------------------------
@@ -94,8 +94,8 @@ CREATE TABLE `commodity`  (
 -- Records of commodity
 -- ----------------------------
 INSERT INTO `commodity` VALUES (1, '酷黑莓桑', 19.00, '喜茶年度口碑王回归。优选桑茸品种，每日手剥去蒂果肉紧致爆汁。新鲜草莓果肉与粒粒桑果肉口感层次叠加，甄选茶园定制绿妍茶汤与桑茸汁、草莓汁一起融合打成冰沙，酸甜果香与清雅茶香轻柔交织。', '喜茶年度口碑王回归。优选桑茸品种，每日手剥去蒂果肉紧致爆汁。', '[{\"title\":\"杯型\",\"option\":[{\"text\":\"酷黑定制杯\",\"cost\":0},{\"text\":\"常规杯\",\"cost\":0}],\"opt\":0},{\"title\":\"状态\",\"option\":[{\"text\":\"冰沙(推荐)\",\"cost\":0},{\"text\":\"非冰沙\",\"cost\":0}],\"opt\":0},{\"title\":\"冰量\",\"option\":[{\"text\":\"推荐\",\"cost\":0},{\"text\":\"少冰\",\"cost\":0},{\"text\":\"少少冰\",\"cost\":0},{\"text\":\"去冰(不推荐)\",\"cost\":0}],\"opt\":0},{\"title\":\"甜度\",\"option\":[{\"text\":\"推荐甜度(少甜)\",\"cost\":0},{\"text\":\"少少甜\",\"cost\":0},{\"text\":\"少少少甜\",\"cost\":0},{\"text\":\"不另外加糖(不推荐)\",\"cost\":0},{\"text\":\"多甜\",\"cost\":0}],\"opt\":0},{\"title\":\"茶底\",\"option\":[{\"text\":\"绿妍(推荐)\",\"cost\":0},{\"text\":\"去茶底\",\"cost\":0}],\"opt\":0},{\"title\":\"加料\",\"option\":[{\"text\":\"不加料\",\"cost\":0},{\"text\":\"加倍桑葚果肉￥4\",\"cost\":4}],\"opt\":0}]', '1', '2023-11-23 17:11:05', '2023-12-03 00:44:14', 'https://dextea-1313412108.cos.ap-guangzhou.myqcloud.com/customer/65656db7152cfcac457bff36-酷黑莓桑.png');
-INSERT INTO `commodity` VALUES (2, '喜柿多多', 19.00, '时令喜柿第二年回归。优选当季甜糯柿子，加入无香精绿妍茶与Q弹的脆波波。入口满满真柿子果肉，果肉软糯拉丝，软籽晶莹弹韧，茶香绵延清雅。喝喜柿，喜柿多。', '喜柿第二年回归。优选当季甜糯柿子，加入绿妍茶与Q弹的脆波波。', '[{\"title\":\"状态\",\"option\":[{\"text\":\"冰沙(推荐)\",\"cost\":0},{\"text\":\"非冰沙\",\"cost\":0}],\"opt\":0},{\"title\":\"冰量\",\"option\":[{\"text\":\"推荐\",\"cost\":0},{\"text\":\"少冰\",\"cost\":0},{\"text\":\"少少冰\",\"cost\":0},{\"text\":\"去冰(不推荐)\",\"cost\":0}],\"opt\":0},{\"title\":\"甜度\",\"option\":[{\"text\":\"推荐甜度(少甜)\",\"cost\":0},{\"text\":\"少少甜\",\"cost\":0},{\"text\":\"少少少甜\",\"cost\":0},{\"text\":\"不另外加糖(不推荐)\",\"cost\":0},{\"text\":\"多甜\",\"cost\":0}],\"opt\":0},{\"title\":\"小料\",\"option\":[{\"text\":\"标准（含脆波波）\",\"cost\":0},{\"text\":\"去脆波波\",\"cost\":0}],\"opt\":0},{\"title\":\"加料\",\"option\":[{\"text\":\"不加料\",\"cost\":0},{\"text\":\"加倍柿子果肉￥4\",\"cost\":4}],\"opt\":0}]', '1', '2023-11-23 20:22:54', '2023-12-02 14:07:05', 'https://dextea-1313412108.cos.ap-guangzhou.myqcloud.com/customer/65656dde152cfcac457bff37-喜柿多多.png');
-INSERT INTO `commodity` VALUES (3, '轻芝多肉葡萄', 19.00, '2022年首创轻芝多肉葡萄。优选当季巨峰葡萄，来自北纬40度黄金产区。鲜果颗颗手剥软嫩多汁，搭配清雅绿妍茶底及弹嫩葡萄冻，定制无奶精芝士，清爽不腻。默认标准杯500ml，可根据个人喜爱选择加大杯650ml.', '2022年首创轻芝多肉葡萄。优选当季巨峰葡萄，来自北纬40度黄金产区。', NULL, '1', '2023-11-23 20:32:27', '2023-12-02 13:50:15', 'https://dextea-1313412108.cos.ap-guangzhou.myqcloud.com/customer/65656db7152cfcac457bff33-轻多肉葡萄.png');
+INSERT INTO `commodity` VALUES (2, '喜柿多多', 19.00, '时令喜柿第二年回归。优选当季甜糯柿子，加入无香精绿妍茶与Q弹的脆波波。入口满满真柿子果肉，果肉软糯拉丝，软籽晶莹弹韧，茶香绵延清雅。喝喜柿，喜柿多。', '喜柿第二年回归。优选当季甜糯柿子，加入绿妍茶与Q弹的脆波波。', '[{\"title\":\"状态\",\"option\":[{\"text\":\"冰沙(推荐)\",\"cost\":0},{\"text\":\"非冰沙\",\"cost\":0}],\"opt\":0},{\"title\":\"冰量\",\"option\":[{\"text\":\"推荐\",\"cost\":0},{\"text\":\"少冰\",\"cost\":0},{\"text\":\"少少冰\",\"cost\":0},{\"text\":\"去冰(不推荐)\",\"cost\":0}],\"opt\":0},{\"title\":\"甜度\",\"option\":[{\"text\":\"推荐甜度(少甜)\",\"cost\":0},{\"text\":\"少少甜\",\"cost\":0},{\"text\":\"少少少甜\",\"cost\":0},{\"text\":\"不另外加糖(不推荐)\",\"cost\":0},{\"text\":\"多甜\",\"cost\":0}],\"opt\":0},{\"title\":\"小料\",\"option\":[{\"text\":\"标准（含脆波波）\",\"cost\":0},{\"text\":\"去脆波波\",\"cost\":0}],\"opt\":0},{\"title\":\"加料\",\"option\":[{\"text\":\"不加料\",\"cost\":0},{\"text\":\"加倍柿子果肉￥4\",\"cost\":4}],\"opt\":0}]', '1', '2023-11-23 20:22:54', '2023-12-05 13:30:47', 'https://dextea-1313412108.cos.ap-guangzhou.myqcloud.com/customer/65656dde152cfcac457bff37-喜柿多多.png');
+INSERT INTO `commodity` VALUES (3, '轻芝多肉葡萄', 19.00, '2022年首创轻芝多肉葡萄。优选当季巨峰葡萄，来自北纬40度黄金产区。鲜果颗颗手剥软嫩多汁，搭配清雅绿妍茶底及弹嫩葡萄冻，定制无奶精芝士，清爽不腻。默认标准杯500ml，可根据个人喜爱选择加大杯650ml.', '2022年首创轻芝多肉葡萄。优选当季巨峰葡萄，来自北纬40度黄金产区。', '[{\"title\":\"状态\",\"option\":[{\"text\":\"冰沙(推荐)\",\"cost\":0},{\"text\":\"非冰沙\",\"cost\":0}],\"opt\":0},{\"title\":\"杯型\",\"option\":[{\"text\":\"标准杯(500ml)\",\"cost\":0},{\"text\":\"加大杯MAX(冷饮650m)￥4\",\"cost\":\"4\"}],\"opt\":0},{\"title\":\"杯盖\",\"option\":[{\"text\":\"透明拱盖\",\"cost\":0},{\"text\":\"白色盖\",\"cost\":0}],\"opt\":0},{\"title\":\"冰量\",\"option\":[{\"text\":\"推荐\",\"cost\":0},{\"text\":\"少冰\",\"cost\":0},{\"text\":\"少少冰\",\"cost\":0},{\"text\":\"去冰(不推荐)\",\"cost\":0}],\"opt\":0},{\"title\":\"甜度\",\"option\":[{\"text\":\"推荐甜度(少甜)\",\"cost\":0},{\"text\":\"少少甜\",\"cost\":0},{\"text\":\"少少少甜\",\"cost\":0},{\"text\":\"不另外加糖(不推荐)\",\"cost\":0},{\"text\":\"多甜\",\"cost\":0}],\"opt\":0},{\"title\":\"茶底\",\"option\":[{\"text\":\"绿妍(推荐)\",\"cost\":0},{\"text\":\"去茶底\",\"cost\":0}],\"opt\":0},{\"title\":\"加料\",\"option\":[{\"text\":\"不加料\",\"cost\":0},{\"text\":\"加倍果肉￥4\",\"cost\":\"4\"}],\"opt\":0},{\"title\":\"小料\",\"option\":[{\"text\":\"标准(含葡萄冻)\",\"cost\":0},{\"text\":\"换弹弹冻\",\"cost\":0},{\"text\":\"去葡萄冻\",\"cost\":0}],\"opt\":0}]', '1', '2023-11-23 20:32:27', '2023-12-05 13:39:00', 'https://dextea-1313412108.cos.ap-guangzhou.myqcloud.com/customer/65656db7152cfcac457bff33-轻多肉葡萄.png');
 INSERT INTO `commodity` VALUES (5, '烤黑糖波波牛乳茶', 19.00, '定制嫣红真茗茶底与无奶精真牛乳灵感特调，加入采用270天自然熟成木薯加工制成的黑糖波波，65分钟慢熬，口感软糯Q弹，不同于普通珍珠;甘醇饱满，入口丝滑。', '定制嫣红真茗茶底与无奶精真牛乳灵感特调', '[{\"title\":\"冰量\",\"option\":[{\"text\":\"推荐\",\"cost\":0},{\"text\":\"少冰\",\"cost\":0},{\"text\":\"少少冰\",\"cost\":0},{\"text\":\"去冰(不推荐)\",\"cost\":0}],\"opt\":0},{\"title\":\"甜度\",\"option\":[{\"text\":\"推荐甜度(少甜)\",\"cost\":0},{\"text\":\"少少甜\",\"cost\":0},{\"text\":\"少少少甜\",\"cost\":0},{\"text\":\"不另外加糖(不推荐)\",\"cost\":0},{\"text\":\"多甜\",\"cost\":0}],\"opt\":0},{\"title\":\"做法\",\"option\":[{\"text\":\"标准(推荐)\",\"cost\":0},{\"text\":\"去芝士(热饮默认去芝士)\",\"cost\":0}],\"opt\":0}]', '1', '2023-11-24 00:19:26', '2023-12-03 00:48:47', 'https://dextea-1313412108.cos.ap-guangzhou.myqcloud.com/customer/65656db7152cfcac457bff35-烤黑糖波波牛乳.png');
 
 -- ----------------------------
@@ -197,7 +197,7 @@ CREATE TABLE `login_log`  (
   `data` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 61 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 74 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of login_log
@@ -262,6 +262,19 @@ INSERT INTO `login_log` VALUES (57, '192.168.205.57', '{\"role\":\"staff\",\"acc
 INSERT INTO `login_log` VALUES (58, '192.168.205.57', '{\"role\":\"staff\",\"account\":\"gzgogo1\"}', '2023-12-04 01:03:28');
 INSERT INTO `login_log` VALUES (59, '192.168.205.57', '{\"role\":\"staff\",\"account\":\"wilson\"}', '2023-12-04 01:14:33');
 INSERT INTO `login_log` VALUES (60, '192.168.205.57', '{\"role\":\"staff\",\"account\":\"gzgogo1\"}', '2023-12-04 01:14:46');
+INSERT INTO `login_log` VALUES (61, '0:0:0:0:0:0:0:1', '{\"role\":\"staff\",\"account\":\"gzgogo1\"}', '2023-12-04 12:57:48');
+INSERT INTO `login_log` VALUES (62, '0:0:0:0:0:0:0:1', '{\"role\":\"staff\",\"account\":\"gzgogo1\"}', '2023-12-04 13:08:55');
+INSERT INTO `login_log` VALUES (63, '0:0:0:0:0:0:0:1', '{\"role\":\"staff\",\"account\":\"wilson\"}', '2023-12-04 13:23:58');
+INSERT INTO `login_log` VALUES (64, '0:0:0:0:0:0:0:1', '{\"role\":\"staff\",\"account\":\"gzgogo1\"}', '2023-12-04 21:16:58');
+INSERT INTO `login_log` VALUES (65, '192.168.205.60', '{\"role\":\"staff\",\"account\":\"gzgogo1\"}', '2023-12-04 21:36:07');
+INSERT INTO `login_log` VALUES (66, '192.168.205.116', '{\"role\":\"staff\",\"account\":\"gzgogo1\"}', '2023-12-04 21:48:58');
+INSERT INTO `login_log` VALUES (67, '192.168.205.57', '{\"role\":\"staff\",\"account\":\"gzgogo1\"}', '2023-12-04 21:51:40');
+INSERT INTO `login_log` VALUES (68, '192.168.180.87', '{\"role\":\"staff\",\"account\":\"gzgogo1\"}', '2023-12-04 21:52:52');
+INSERT INTO `login_log` VALUES (69, '192.168.205.57', '{\"role\":\"staff\",\"account\":\"gzgogo1\"}', '2023-12-04 22:46:37');
+INSERT INTO `login_log` VALUES (70, '0:0:0:0:0:0:0:1', '{\"role\":\"staff\",\"account\":\"gzgogo1\"}', '2023-12-04 23:55:04');
+INSERT INTO `login_log` VALUES (71, '0:0:0:0:0:0:0:1', '{\"role\":\"staff\",\"account\":\"gzgogo1\"}', '2023-12-05 00:37:45');
+INSERT INTO `login_log` VALUES (72, '0:0:0:0:0:0:0:1', '{\"role\":\"staff\",\"account\":\"wilson\"}', '2023-12-05 13:30:22');
+INSERT INTO `login_log` VALUES (73, '0:0:0:0:0:0:0:1', '{\"role\":\"staff\",\"account\":\"gzgogo1\"}', '2023-12-05 14:03:35');
 
 -- ----------------------------
 -- Table structure for order
@@ -281,13 +294,14 @@ CREATE TABLE `order`  (
   `note` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL COMMENT '备注',
   `code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '取餐码',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of order
 -- ----------------------------
-INSERT INTO `order` VALUES (4, 1, 1, 2, 38.00, 2, '[{\"id\":1,\"name\":\"酷黑莓桑\",\"price\":19,\"num\":1,\"img\":\"https://dextea-1313412108.cos.ap-guangzhou.myqcloud.com/customer/65656db7152cfcac457bff36-酷黑莓桑.png\",\"custom\":[{\"item\":\"杯型\",\"value\":\"酷黑定制杯\"},{\"item\":\"状态\",\"value\":\"冰沙(推荐)\"},{\"item\":\"冰量\",\"value\":\"推荐\"},{\"item\":\"甜度\",\"value\":\"推荐甜度(少甜)\"},{\"item\":\"茶底\",\"value\":\"绿妍(推荐)\"},{\"item\":\"加料\",\"value\":\"不加料\"}]},{\"id\":2,\"name\":\"喜柿多多\",\"price\":19,\"num\":1,\"img\":\"https://dextea-1313412108.cos.ap-guangzhou.myqcloud.com/customer/65656dde152cfcac457bff37-喜柿多多.png\",\"custom\":[{\"item\":\"杯型\",\"value\":\"酷黑定制杯\"},{\"item\":\"状态\",\"value\":\"冰沙(推荐)\"},{\"item\":\"冰量\",\"value\":\"推荐\"},{\"item\":\"甜度\",\"value\":\"推荐甜度(少甜)\"},{\"item\":\"茶底\",\"value\":\"绿妍(推荐)\"},{\"item\":\"加料\",\"value\":\"不加料\"}]}]', '2023-12-04 00:42:15', '2023-12-04 00:42:15', '13570790338', '备注', '8003');
-INSERT INTO `order` VALUES (5, 1, 1, 2, 38.00, 2, '[{\"id\":1,\"name\":\"酷黑莓桑\",\"price\":19,\"num\":1,\"img\":\"https://dextea-1313412108.cos.ap-guangzhou.myqcloud.com/customer/65656db7152cfcac457bff36-酷黑莓桑.png\",\"custom\":[{\"item\":\"杯型\",\"value\":\"酷黑定制杯\"},{\"item\":\"状态\",\"value\":\"冰沙(推荐)\"},{\"item\":\"冰量\",\"value\":\"推荐\"},{\"item\":\"甜度\",\"value\":\"推荐甜度(少甜)\"},{\"item\":\"茶底\",\"value\":\"绿妍(推荐)\"},{\"item\":\"加料\",\"value\":\"不加料\"}]},{\"id\":2,\"name\":\"喜柿多多\",\"price\":19,\"num\":1,\"img\":\"https://dextea-1313412108.cos.ap-guangzhou.myqcloud.com/customer/65656dde152cfcac457bff37-喜柿多多.png\",\"custom\":[{\"item\":\"杯型\",\"value\":\"酷黑定制杯\"},{\"item\":\"状态\",\"value\":\"冰沙(推荐)\"},{\"item\":\"冰量\",\"value\":\"推荐\"},{\"item\":\"甜度\",\"value\":\"推荐甜度(少甜)\"},{\"item\":\"茶底\",\"value\":\"绿妍(推荐)\"},{\"item\":\"加料\",\"value\":\"不加料\"}]}]', '2023-12-04 00:42:18', '2023-12-04 00:42:18', '13570790338', '备注', '8004');
+INSERT INTO `order` VALUES (4, 1, 1, 3, 38.00, 2, '[{\"id\":1,\"name\":\"酷黑莓桑\",\"price\":19,\"num\":1,\"img\":\"https://dextea-1313412108.cos.ap-guangzhou.myqcloud.com/customer/65656db7152cfcac457bff36-酷黑莓桑.png\",\"custom\":[{\"item\":\"杯型\",\"value\":\"酷黑定制杯\"},{\"item\":\"状态\",\"value\":\"冰沙(推荐)\"},{\"item\":\"冰量\",\"value\":\"推荐\"},{\"item\":\"甜度\",\"value\":\"推荐甜度(少甜)\"},{\"item\":\"茶底\",\"value\":\"绿妍(推荐)\"},{\"item\":\"加料\",\"value\":\"不加料\"}]},{\"id\":2,\"name\":\"喜柿多多\",\"price\":19,\"num\":1,\"img\":\"https://dextea-1313412108.cos.ap-guangzhou.myqcloud.com/customer/65656dde152cfcac457bff37-喜柿多多.png\",\"custom\":[{\"item\":\"杯型\",\"value\":\"酷黑定制杯\"},{\"item\":\"状态\",\"value\":\"冰沙(推荐)\"},{\"item\":\"冰量\",\"value\":\"推荐\"},{\"item\":\"甜度\",\"value\":\"推荐甜度(少甜)\"},{\"item\":\"茶底\",\"value\":\"绿妍(推荐)\"},{\"item\":\"加料\",\"value\":\"不加料\"}]}]', '2023-12-04 00:42:15', '2023-12-05 13:51:07', '13570790338', '备注', '8003');
+INSERT INTO `order` VALUES (5, 1, 1, 3, 38.00, 2, '[{\"id\":1,\"name\":\"酷黑莓桑\",\"price\":19,\"num\":1,\"img\":\"https://dextea-1313412108.cos.ap-guangzhou.myqcloud.com/customer/65656db7152cfcac457bff36-酷黑莓桑.png\",\"custom\":[{\"item\":\"杯型\",\"value\":\"酷黑定制杯\"},{\"item\":\"状态\",\"value\":\"冰沙(推荐)\"},{\"item\":\"冰量\",\"value\":\"推荐\"},{\"item\":\"甜度\",\"value\":\"推荐甜度(少甜)\"},{\"item\":\"茶底\",\"value\":\"绿妍(推荐)\"},{\"item\":\"加料\",\"value\":\"不加料\"}]},{\"id\":2,\"name\":\"喜柿多多\",\"price\":19,\"num\":1,\"img\":\"https://dextea-1313412108.cos.ap-guangzhou.myqcloud.com/customer/65656dde152cfcac457bff37-喜柿多多.png\",\"custom\":[{\"item\":\"杯型\",\"value\":\"酷黑定制杯\"},{\"item\":\"状态\",\"value\":\"冰沙(推荐)\"},{\"item\":\"冰量\",\"value\":\"推荐\"},{\"item\":\"甜度\",\"value\":\"推荐甜度(少甜)\"},{\"item\":\"茶底\",\"value\":\"绿妍(推荐)\"},{\"item\":\"加料\",\"value\":\"不加料\"}]}]', '2023-12-04 00:42:18', '2023-12-05 13:51:09', '13570790338', '备注', '8004');
+INSERT INTO `order` VALUES (7, 1, 1, 2, 23.00, 1, '[{\"id\":1,\"name\":\"酷黑莓桑\",\"price\":23,\"img\":\"https://dextea-1313412108.cos.ap-guangzhou.myqcloud.com/customer/65656db7152cfcac457bff36-酷黑莓桑.png\",\"num\":1,\"custom\":[{\"item\":\"杯型\",\"value\":\"酷黑定制杯\"},{\"item\":\"状态\",\"value\":\"冰沙(推荐)\"},{\"item\":\"冰量\",\"value\":\"推荐\"},{\"item\":\"甜度\",\"value\":\"推荐甜度(少甜)\"},{\"item\":\"茶底\",\"value\":\"绿妍(推荐)\"},{\"item\":\"加料\",\"value\":\"加倍桑葚果肉￥4\"}]}]', '2023-12-05 14:03:24', '2023-12-05 14:03:48', '13570790338', '', '8002');
 
 -- ----------------------------
 -- Table structure for setting
@@ -384,7 +398,7 @@ CREATE TABLE `store`  (
 -- ----------------------------
 -- Records of store
 -- ----------------------------
-INSERT INTO `store` VALUES (1, '广州gogo新天地店', '[\"广东省\",\"广州市\"]', '小谷围街贝岗村贝岗村大街1号高高新天地商业广场一期一层第103、104、141、142号商铺', '020-85202718', '周一至周日 10:00-22:00', '1', '2023-11-02 23:47:28', '2023-12-02 00:39:24');
+INSERT INTO `store` VALUES (1, '广州gogo新天地店', '[\"广东省\",\"广州市\"]', '小谷围街贝岗村贝岗村大街1号高高新天地商业广场一期一层第103、104、141、142号商铺', '020-85202718', '周一至周日 10:00-22:00', '1', '2023-11-02 23:47:28', '2023-12-04 13:12:19');
 INSERT INTO `store` VALUES (2, '广州万胜广场店', '[\"广东省\", \"广州市\"]', '广东省广州市海珠区新港东路1236号101铺', '020-80927484', '周一至周四 10:00-21:00，周五至周日 10:00-22:00', '2', '2023-11-04 02:03:11', '2023-12-04 00:50:15');
 INSERT INTO `store` VALUES (3, '广州天环广场LAB店', '[\"广东省\",\"广州市\"]', '广州市天河区天河路218号天环广场地上一层 址L105、地上二层L205号商铺', '020-85201268', '周一至周四 09:00-22:00，周五至周日 09:00-22:30', '1', '2023-11-20 23:55:10', '2023-12-02 00:39:24');
 INSERT INTO `store` VALUES (4, '广州琶洲保利广场店', '[\"广东省\",\"广州市\"]', '琶洲蟠龙新街2号保利广 场购物中心1层1005、1006商铺', '020-85208728', '周一至周日 10:00-23:00', '1', '2023-11-21 00:04:25', '2023-12-02 00:39:24');
@@ -407,7 +421,6 @@ CREATE TABLE `store_comm`  (
 -- Records of store_comm
 -- ----------------------------
 INSERT INTO `store_comm` VALUES (1, 1, '2023-12-04 01:03:21');
-INSERT INTO `store_comm` VALUES (1, 2, '2023-12-04 01:03:21');
 INSERT INTO `store_comm` VALUES (1, 3, '2023-12-04 01:03:21');
 INSERT INTO `store_comm` VALUES (1, 5, '2023-12-04 01:03:21');
 INSERT INTO `store_comm` VALUES (2, 1, '2023-12-03 00:34:58');
@@ -443,7 +456,7 @@ CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `menu` AS select `store`.
 -- View structure for menu_unwind
 -- ----------------------------
 DROP VIEW IF EXISTS `menu_unwind`;
-CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `menu_unwind` AS select `menu`.`store_id` AS `store_id`,`menu`.`store_name` AS `store_name`,`menu`.`id` AS `id`,`menu`.`name` AS `name`,`menu`.`price` AS `price`,`menu`.`introduce` AS `introduce`,`menu`.`brief_intro` AS `brief_intro`,`menu`.`custom` AS `custom`,`menu`.`img` AS `img`,`menu`.`sell_state` AS `sell_state`,`category`.`id` AS `cate_id`,`category`.`name` AS `cate_name` from ((`menu` left join `cate2comm` on((`menu`.`id` = `cate2comm`.`comm_id`))) left join `category` on((`cate2comm`.`cate_id` = `category`.`id`)));
+CREATE ALGORITHM = UNDEFINED SQL SECURITY DEFINER VIEW `menu_unwind` AS select `menu`.`store_id` AS `store_id`,`menu`.`store_name` AS `store_name`,`menu`.`id` AS `id`,`menu`.`name` AS `name`,`menu`.`price` AS `price`,`menu`.`introduce` AS `introduce`,`menu`.`brief_intro` AS `brief_intro`,`menu`.`custom` AS `custom`,`menu`.`img` AS `img`,`category`.`id` AS `cate_id`,`category`.`name` AS `cate_name`,`menu`.`state` AS `state` from ((`menu` left join `cate2comm` on((`menu`.`id` = `cate2comm`.`comm_id`))) left join `category` on((`cate2comm`.`cate_id` = `category`.`id`)));
 
 -- ----------------------------
 -- View structure for order_view
